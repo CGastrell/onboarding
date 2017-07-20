12f03bdf7450d3f0b8861a65c1781baa1117495f
Christian Gastrell <cgastrell@gmail.com>
2017-07-20 16:35:21 -0300

    limit zoom level and change osm tile origins

--- src/view/map/index.js
+++ src/view/map/index.js
@@ -48,14 +48,16 @@ export default View.extend({
   setupMap: function () {
     this.hybridLayer = window.MQ.hybridLayer()
     this.satelliteLayer = window.MQ.satelliteLayer()
-    this.plainLayer = L.tileLayer('http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png', {
+    this.plainLayer = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
       maxZoom: 18,
-      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+      attribution: 'Map data © <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
     })
     this.drawLayer = new L.GeoJSON()
     this.map = L.map(this.el, {
       layers: [this.drawLayer, this.hybridLayer],
       center: [ App.state.mapstate.center.lat, App.state.mapstate.center.lng ],
+      minZoom: 2,
+      maxZoom: 17,
       zoom: App.state.mapstate.zoom
     })
 
